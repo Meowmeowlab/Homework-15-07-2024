6ca25b2f75b6130cc994c9fea9eff946f3bf4839
Meowmeowlab <174057741+Meowmeowlab@users.noreply.github.com>
2024-07-14 01:05:36 +0700

    Adding question 4

---
+++ B4-10-07-2024/B4.py
@@ -0,0 +1,32 @@
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.fft import fft2, fftshift
+
+
+img = cv2.imread('./testimg/meow_400p.jpg', cv2.IMREAD_GRAYSCALE)
+
+if img is None:
+    raise ValueError("Không thể đọc được ảnh. Hãy kiểm tra lại đường dẫn.")
+
+# FFT - Fourier
+dft = fft2(img)
+dft_shift = fftshift(dft)
+
+# Calc frequency spectrum
+magnitude_spectrum = 20 * np.log(np.abs(dft_shift))
+
+# Display
+plt.figure(figsize=(12, 6))
+
+plt.subplot(1, 2, 1)
+plt.imshow(img, cmap='gray')
+plt.title('Original Image')
+plt.axis('off')
+
+plt.subplot(1, 2, 2)
+plt.imshow(magnitude_spectrum, cmap='gray')
+plt.title('Frequency Spectrum')
+plt.axis('off')
+
+plt.show()
